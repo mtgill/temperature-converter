f6731cba1ef73bc8b27cb3f9580f3b93d64c572f
Matt Gill <mtgill90@outlook.com>
2019-03-17 16:07:44 -0500

    temp conversion working, printing to outputDiv

--- main.js
+++ main.js
@@ -1,1 +1,65 @@
-console.log('hi');
+
+let finalTemp = 0;
+const toCelsius =  (temp) => {
+    const tempC = (temp - 32) * (5/9);
+    finalTemp = tempC;
+ };
+
+
+
+const toFahrenheit = (temp) => {
+    const tempF = temp * (9/5) + 32;
+    finalTemp = tempF;
+};
+
+
+// Print to DOM function 
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+}
+
+// Get a reference to the button element in the DOM
+const button = document.getElementById("convertBtn");
+
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+
+let temp = document.getElementById('tempInput').value;
+let unit = "";
+
+const determineConverter = (e) => {
+
+    const temp = document.getElementById('tempInput').value;
+
+
+    if (document.getElementById('tempC').checked === true) {
+    unit = 'C'
+    toCelsius(temp);
+    domStringBuilder(finalTemp, unit);
+    }
+    else if (document.getElementById('tempF').checked === true) {
+        unit = 'F'
+        toFahrenheit(temp);
+        domStringBuilder(finalTemp, unit);
+    }
+
+    //tempValue = document.getElementById('tempInput').value;
+    //printToDom('tempOutput', tempValue);
+};
+
+// Assign a function to be executed when the button is clicked
+
+
+button.addEventListener("click", determineConverter);
+
+domString = "";
+const domStringBuilder = (temperature, unitOfTemp) => {
+    domString += `${finalTemp} degrees ${unit}`;
+    printToDom('tempOutput', domString);
+};
+
+
+
+
